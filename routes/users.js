537e62d90480232846ae7d9291609d94108fd152
juyun <juyun571@naver.com>
2024-12-18 17:28:12 +0900

    feat: 정보수정 페이지 추가

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.get("/", controller.users); // users
 router.get("/login", controller.login); // login
 router.get("/edit", controller.edit); // usersedit
 router.get("/register", controller.register); // register
-router.get("/users/details", controller.userProfile);
+router.get("/details", controller.userProfile);
 
 // POST
 router.post("/login", controller.userLogin); //login post
