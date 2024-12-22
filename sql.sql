ae0dcc616ca4f1059f0f989e3909a82d9698fde4
kim-chulsoon <nicnk90@gmail.com>
2024-12-22 23:04:39 +0900

    241222

--- sql.sql
+++ sql.sql
@@ -3,4 +3,10 @@ create DATABASE meal;
 use meal;
 
 CREATE USER 'sesac'@'%' IDENTIFIED BY '1234';
+show tables;
+desc notes;
 
+select * from notes;
+SELECT * 
+FROM Notes
+WHERE videoId = '1' AND userId = 'asdf1234';
