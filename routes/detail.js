b4776d8ac4dc9d33f03ba3d8089f033d8ad01296
kim-chulsoon <nicnk90@gmail.com>
2024-12-23 12:09:48 +0900

    작업내용 저장

--- routes/detail.js
+++ routes/detail.js
@@ -6,6 +6,8 @@ const authenticateToken = require("../middlewares/authMiddleware");
 // GET
 router.get("/", controller.detail); // detail
 // POST
-router.post("/notes", authenticateToken, controller.Notes);
+router.post("/notes", authenticateToken, controller.CreateNotesNotes); // 메모 생성
+router.patch("/notes/:id", authenticateToken, notesController.updateNote); // 메모 수정
+router.delete("/notes/:id", authenticateToken, notesController.deleteNote); // 메모 삭제
 
 module.exports = router;
